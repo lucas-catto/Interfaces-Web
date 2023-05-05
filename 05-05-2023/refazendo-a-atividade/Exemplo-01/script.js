7c628f9d93d16a3322897a31d560835e24df9375
Lucas Silva <lucascattosilva2018@gmail.com>
2023-05-05 12:27:53 -0300

    Alterei o HTML, CSS e Javascript.

---
+++ 05-05-2023/refazendo-a-atividade/Exemplo-01/script.js
@@ -0,0 +1,19 @@
+const carrosselContainer = document.querySelector('.carrossel-container');
+const carrosselImagens   = carrosselContainer.querySelectorAll('.carrossel-imagem');
+
+let imagemAtual = 0;
+
+const alturaImagemAtiva         = carrosselImagens[imagemAtual].getBoundingClientRect().height;
+carrosselContainer.style.height = `${alturaImagemAtiva}px`;
+
+function mostrarProximaImagem(){
+    
+    carrosselImagens[imagemAtual].classList.remove('ativa');
+    imagemAtual++;
+    if (imagemAtual >= carrosselImagens.length) {
+        imagemAtual = 0;
+    }
+    carrosselImagens[imagemAtual].classList.add('ativa');
+}
+
+setInterval(mostrarProximaImagem, 450);
