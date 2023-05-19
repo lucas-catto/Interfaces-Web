06dc6fc4667dbb47b0e835cd6fd115f15871897a
Lucas Silva <lucascattosilva2018@gmail.com>
2023-05-19 12:24:22 -0300

    Incrementei a validação do teclado numérico.

--- 19-05-2023/Lista01-2BIM/ex02/script.js
+++ 19-05-2023/Lista01-2BIM/ex02/script.js
@@ -1,6 +1,6 @@
 function validaNumero(e){
     var charCode = (e.which) ? e.which : e.keyCode;
-    if((charCode >= 48 && charCode <= 57) || charCode == 8){
+    if((charCode >= 48 && charCode <= 57) || charCode == 8 || (charCode >= 96 && charCode <= 105)){
         return true;
     }
     else{ return false; }
